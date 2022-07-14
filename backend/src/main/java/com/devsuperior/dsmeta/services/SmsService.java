7574d07862bbc30678340a48bce66781635a30c7
joosecj <josecarloscjj@gmail.com>
2022-07-14 12:18:28 -0300

    Heroku att

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -19,7 +19,6 @@ public class SmsService {
     private String twilioPhoneFrom;
     @Value("${twilio.phone.to}")
     private String twilioPhoneTo;
-
     @Autowired
     private SaleRepository saleRepository;
     public void sendSms(Long saleId) {
@@ -27,7 +26,8 @@ public class SmsService {
         String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
         String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date
-                + " com um total de R$ " + String.format("%.2f", sale.getAmount());
+                + " com um total de R$ " + String.format("%.0f", sale.getAmount());
+
 
         Twilio.init(twilioSid, twilioKey);
 
